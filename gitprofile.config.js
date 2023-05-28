f7ea2b8d8f77c6e09936c036e0b8ee4c13d0a303
wonyounglee <lwy970327@naver.com>
2023-05-28 20:31:04 +0900

    fourht

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -100,9 +100,9 @@ const config = {
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: '', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
+    // source: 'dev', // medium | dev
+    // username: '', // to hide blog section, keep it empty
+    // limit: 2, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
