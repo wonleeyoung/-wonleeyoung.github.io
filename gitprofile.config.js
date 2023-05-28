51d1e22ace5456807be650fb6b5993a2400ebe27
wonyounglee <lwy970327@naver.com>
2023-05-28 16:02:24 +0900

    hi

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,9 +2,9 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'wonleeyoung', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
+    limit: 8, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
